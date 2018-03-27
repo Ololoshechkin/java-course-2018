9914e2a88203975679e032c6272b53a2678d6517
Vadim Brilyantov <v.brilyantov@yandex.ru>
2018-03-27 18:34:24 +0300

    jar_gen

--- jar5.sh
+++ jar5.sh
@@ -1,4 +1,10 @@
-javac -d production/ -cp ./classes:./artifacts/JarImplementorTest.jar:./lib/hamcrest-core-1.3.jar:./lib/jsoup-1.8.1.jar:./lib/junit-4.11.jar:./lib/quickcheck-0.6.jar src/java/ru/ifmo/rain/brilyantov/implementor/Implementor.java
+#!/bin/bash
+JAR_TESTER="artifacts/JarImplementorTest.jar"
+MY_PACKAGE="ru/ifmo/rain/brilyantov/implementor"
+KGEORGY_PACKAGE="info/kgeorgiy/java/advanced/implementor"
+
+javac -d production/ -cp ./${JAR_TESTER} src/java/${MY_PACKAGE}/Implementor.java
 cd production/
-jar cfm Implementor.jar ../Manifest.txt ru/ifmo/rain/brilyantov/implementor/Implementor.class
+jar xvf ../${JAR_TESTER} ${KGEORGY_PACKAGE}/Impler.class ${KGEORGY_PACKAGE}/JarImpler.class ${KGEORGY_PACKAGE}/ImplerException.class
+jar cfe Implementor.jar ru.ifmo.rain.brilyantov.implementor.Implementor ${MY_PACKAGE}/*.class ${KGEORGY_PACKAGE}/*.class
 cd ..
