57ac4c66694bff82520c4644c6ed9e192c2720fd
Vadim Brilyantov <v.brilyantov@yandex.ru>
2018-03-28 14:05:08 +0300

    package info

---
+++ src/java/ru/ifmo/rain/brilyantov/implementor/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing {@link ru.ifmo.rain.brilyantov.implementor.Implementor} class for implementing given java-classes
+ * or interfaces
+ */
+
+package ru.ifmo.rain.brilyantov.implementor;
